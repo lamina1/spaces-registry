ea3551d6221fbcd162bce414074dd386a2e73dff
Bernardo Cardoso <29538426+bmastc@users.noreply.github.com>
2023-10-23 16:50:25 +0000

    Update metadata link. Add spaces testnet config

--- scripts/deploy.ts
+++ scripts/deploy.ts
@@ -41,12 +41,12 @@ async function main() {
   const trophyFactory = await ethers.getContractFactory("SpaceLasersTrophy");
   const items: [string, BaseItem__factory][] = [
     [
-      "ipfs://bafybeigv74z6kzg5rvhlx5s2c7vrmgm5dhi7fisoqeflv7mvyccvolezwe/",
+      "ipfs://bafybeifnr7ibo27ux2ibdpeawvdrr3wnggjqqn2jstc4rjfrqrsvqlgb5q/",
       itemFactory,
     ],
   ];
   const trophy: [string, BaseItem__factory] = [
-    "ipfs://bafybeigv74z6kzg5rvhlx5s2c7vrmgm5dhi7fisoqeflv7mvyccvolezwe/trophy.json",
+    "ipfs://bafybeifnr7ibo27ux2ibdpeawvdrr3wnggjqqn2jstc4rjfrqrsvqlgb5q/trophy.json",
     trophyFactory,
   ];
 
